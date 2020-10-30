c18dd9bc7c55b548bb779ab3ad12b00d63253ef1
sampathdissanayaka <dmcsampath97@gmail.com>
2020-10-30 23:13:59 +0700

    update appvue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div id="app">
+
    <navbar></navbar>
+   <router-view></router-view>
   </div>
 </template>
 
 <script>
 
 import Navbar from './components/Navbar.vue'
+
 export default {
 
  components:{
