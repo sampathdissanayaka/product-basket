44111f004f263f2f86889ac4863e2c4e6a7495da
sampathdissanayaka <dmcsampath97@gmail.com>
2020-10-30 23:13:41 +0700

    create routes

---
+++ src/routes.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+import AddProduct from './components/AddProduct.vue'
+import ProductBasket from './components/ProductBasket.vue'
+
+const router = new VueRouter({
+ 
+
+    mode:'history',
+
+    routes:[
+ 
+        {path:"/add",component:AddProduct},
+        {paht:"/basket",component:ProductBasket}
+        
+ ]
+
+});
+
+export default router
